4309ee1ad0a6cdc2f6b09f2fd33b4b22ca12955d
Jakob Walmann <jakob.walmann@outlook.com>
2022-11-16 14:22:27 +0100

    Initial test functions created

--- src/test/kotlin/com/celestar/FunctionTest.kt
+++ src/test/kotlin/com/celestar/FunctionTest.kt
@@ -17,30 +17,6 @@ class FunctionTest {
     inline fun <reified T : Any> mock() = mock(T::class.java)
 
     private fun testHttpTrigger(httpMethod: HttpMethod) {
-        // Setup
-        val req = mock<HttpRequestMessage<Optional<String>>>()
-
-        val queryParams = HashMap<String, String>()
-        queryParams["name"] = "Azure"
-        doReturn(queryParams).`when`<HttpRequestMessage<Optional<String>>>(req).queryParameters
-
-        val queryBody = Optional.empty<String>()
-        doReturn(queryBody).`when`<HttpRequestMessage<*>>(req).body
-        doReturn(httpMethod).`when`<HttpRequestMessage<*>>(req).httpMethod
-
-        doAnswer { invocation ->
-            val status = invocation.arguments[0] as HttpStatus
-            HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status)
-        }.`when`<HttpRequestMessage<*>>(req).createResponseBuilder(any(HttpStatus::class.java))
-
-        val context = mock(ExecutionContext::class.java)
-        doReturn(Logger.getGlobal()).`when`(context).logger
-
-        // Invoke
-        val ret = Function().run(req, context)
-
-        // Verify
-        assertEquals(ret.status, HttpStatus.OK)
     }
 
     /**
